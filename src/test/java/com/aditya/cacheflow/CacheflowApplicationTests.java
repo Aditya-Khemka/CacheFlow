db1989639dcfc70bd467d53d3eff9d9bfdb73afe
Aditya Khemka <khemkaaditya48@gmail.com>
2026-09-13 22:02:58 +0530

    init

---
+++ src/test/java/com/aditya/cacheflow/CacheflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.cacheflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
